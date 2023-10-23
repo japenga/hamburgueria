053b48454872ec4a026ebc32b2622bd982a495e7
japenga <105770458+japenga@users.noreply.github.com>
2023-10-23 17:30:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,11 +7,16 @@ function voltar(){
 
 const elemento1 = document.getElementById("header");
 const elemento2 = document.getElementById("header2");
+const elemento3 = document.getElementById("drop");
 
 function itens(){
     elemento2.style.visibility = "visible";
     elemento2.style.transition = ".1s";
+    
     }
 function sair(){
     elemento2.style.visibility = "hidden";
+}
+function menuShow(){
+    elemento3.style.display = "block";
 }
